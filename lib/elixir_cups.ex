9d183257347646de7f3491e4f8e7de322518efa5
Bruno Santana Alves <bruno.sant.a@gmail.com>
2019-01-27 00:03:21 -0300

    Add printer

--- lib/elixir_cups.ex
+++ lib/elixir_cups.ex
@@ -24,13 +24,19 @@ defmodule ElixirCups do
   def list_printers(), do: GenServer.call(__MODULE__, :list)
   def sel_printer(printer), do: GenServer.cast(__MODULE__, {:printer, printer})
   def print(file), do: GenServer.cast(__MODULE__, {:print, file})
+  def add_printer(name, ip \\ ""), do: GenServer.cast(__MODULE__, {:add, name, ip})
   
   def handle_cast({:printer, printer}, state) do
     {:noreply, %{state | :printer => printer}}  
   end
 
   def handle_cast({:print, file}, state) do
-    System.cmd("lpr", ["-P", state.printer, file])
+    System.cmd("lp", ["-d", state.printer, "-o", "document-format=text/html", file])
+    {:noreply, state}
+  end
+
+  def handle_cast({:add, name, ip}, state) do
+    System.cmd("lpadmin", ["-p", name, "-E", "-v", "ipp://#{ip}/ipp/print", "-m", "everywhere"])
     {:noreply, state}
   end
 
